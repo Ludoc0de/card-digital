ecf0b135cd04ba26374d43bd8226d1b3aab9197e
Asakuracl <contact@neographe.org>
2022-04-08 23:30:06 -0300

    transfer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css'
+import React from "react"
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Card from './components/Card'
+import Footer from './components/Footer'
+import data from './data'
 
 function App() {
+
+  const[page, setPage]= React.useState(true)
+  const[menu, setMenu]= React.useState(true)
+
+  function toggle() {
+    setPage(prevPage => !prevPage)
+  }
+
+  function Menu() {
+    setMenu(prevMenu => !prevMenu)
+  }
+
+  const cardAppear = {
+    top: page ? "100%" :"8%",
+    transition: "0.5s ease-in 0.5s"
+  }
+
+  const cards = data.map(item => {
+    return(
+      <Card 
+        key={item.id} 
+        image= {item.image}
+        title={item.title}
+        description={item.description}
+        website={item.website}
+        firstSkill={item.skill.first}
+        secondSkill={item.skill.second}
+        thirdSkill={item.skill.third}
+        fourthSkill={item.skill.fourth}
+        firstContact={item.contact.first}
+        secondContact={item.contact.second}
+        thirdContact={item.contact.third}
+        fourthContact={item.contact.fourth}
+        fifthContact={item.contact.fifth}
+        // {...item}
+      />
+    )
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header handleClick={toggle} menuClick={Menu} burger={menu}/> 
+      <Hero emerge={page}/>
+      <section className='card' style={cardAppear}>
+        {cards}
+      </section>
+      <Footer />
     </div>
   );
 }
